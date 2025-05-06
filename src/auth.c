510b66f7f78fd2f6ea5b0e4f47ff00609858c716
wqqwdwqd <167070630+wqqwdwqd@users.noreply.github.com>
2025-05-06 21:14:29 +0300

    Delete src directory

--- src/auth.c
+++
@@ -1,9 +0,0 @@
-#include "auth.h"
-
-int authenticate_user(const char* username, const char* password) {
-    // Простая проверка имени пользователя и пароля
-    if (strcmp(username, "admin") == 0 && strcmp(password, "password") == 0) {
-        return 1; // Успешная аутентификация
-    }
-    return 0; // Неуспешная аутентификация
-}
